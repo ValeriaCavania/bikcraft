6ec3977d9aa373de2da82726fc97314e6dc14acd
Valeria da Silva Cavania <1485655@ifms.edu.br>
2026-04-14 15:26:49 -0400

    perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ function ativarProduto(parametro) {
 parametros.forEach(ativarProduto);
 
 //Perguntas frequentes
-
 const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
